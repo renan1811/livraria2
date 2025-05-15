964e6fd3676514136de7f66ad8eacafa58148cec
renan1811 <rennlima477@gmail.com>
2025-05-15 13:58:38 -0300

    criação da livraria e class categoria e editora

---
+++ livraria/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+# começandoooo
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.descricao
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return  self.nome
