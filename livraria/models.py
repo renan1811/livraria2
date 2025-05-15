2464afee9f1b55fe2d53e8bf35d08246e2164a5c
renan1811 <rennlima477@gmail.com>
2025-05-15 14:22:53 -0300

    Formatação dos arquivos

--- livraria/models.py
+++ livraria/models.py
@@ -1,17 +1,20 @@
 from django.db import models
+
 # começandoooo
+
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
     nome = models.CharField(max_length=100)
     site = models.URLField(null=True, blank=True)
-    
+
     def __str__(self):
         return self.descricao
+
 
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
     site = models.URLField(null=True, blank=True)
 
     def __str__(self):
-        return  self.nome
+        return self.nome
