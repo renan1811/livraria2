1c746ee975b06695dc395cdce1842338a1b453b9
renan1811 <rennlima477@gmail.com>
2025-05-15 16:49:11 -0300

    adição das class Autor e Livro

--- livraria/admin.py
+++ livraria/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Categoria
 from .models import Editora
+from .models import Autor
+from .models import Livro
+
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livro)
