be31447e7b9c7bf4a663a2ac2f948306ca8c964f
renan1811 <rennlima477@gmail.com>
2025-06-20 08:50:31 -0300

    terminei aula 20

--- livraria/admin.py
+++ livraria/admin.py
@@ -3,7 +3,31 @@ from django.contrib import admin
 # Register your models here.
 from .models import Categoria, Autor, Livro, Editora
 
-admin.site.register(Categoria)
-admin.site.register(Editora)
-admin.site.register(Autor)
-admin.site.register(Livro)
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+    search_fields = ('nome', 'email')
+    list_filter = ('nome',)
+    ordering = ('nome', 'email')
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('descricao',)
+    search_fields = ('descricao',)
+    list_filter = ('descricao',)
+    ordering = ('descricao',)
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+    list_filter = ('nome',)
+    ordering = ('nome',)
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'editora', 'categoria')
+    search_fields = ('nome', 'editora__nome', 'categoria__descricao')
+    list_filter = ('editora', 'categoria')
+    ordering = ('nome', 'editora', 'categoria')
+    list_per_page = 25
