c9b3618bbfe30cea369ea96570e48090df7b0bd8
renan1811 <rennlima477@gmail.com>
2025-06-21 13:47:52 -0300

    terminei aula 28

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -1,10 +1,22 @@
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
+from livraria.models import Compra, ItensCompra
 
-from livraria.models import Compra
+class ItensCompraSerializer(ModelSerializer):
+    total = SerializerMethodField()
+
+    class Meta:
+        model = ItensCompra
+        fields = ["livro", "quantidade", "total"]
+        depth = 2
+
+    def get_total(self, instance):
+        return instance.quantidade * instance.livro.preco
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True)
     status = CharField(source="get_status_display", read_only=True)
+    itens = ItensCompraSerializer(many=True, read_only=True)
+
     class Meta:
         model = Compra
         fields = "__all__"
